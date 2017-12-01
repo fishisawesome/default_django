1565cbafa7b0f1a7b9bef7edcc661fd626bf9da6
Carlo Villamayor <v@Carlos-MacBook-Pro.local>
2017-12-02 05:40:46 +0800

    add templates directory

--- settings/production.py
+++ settings/production.py
@@ -54,14 +54,16 @@ ROOT_URLCONF = 'urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
+                'django.template.context_processors.i18n',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
